a962e26e86d90f6d41adab766857376ffa1a4364
Matt Bresnan <bresnan.mw@gmail.com>
2016-03-10 08:59:34 -0800

    Improved algorithm

--- src/solvers.js
+++ src/solvers.js
@@ -20,33 +20,35 @@ window.findNRooksSolution = function(n) {
   var board = new Board({'n': n });
 
   var checkBoard = function (row) {
+    while(solution.length === 0){
 
-    if (row === n) { // base case: last rows in board 
-      for(var i = 0; i < n; i++) {
-        solution.push(board.get(i));
 
+      if (row === n) { // base case: last rows in board 
+        for(var i = 0; i < n; i++) {
+          solution.push(board.get(i));
+
+        }
+        console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+        return solution;
+
+      }  
+      for (var i = 0; i < n; i++) {  
+
+        board.togglePiece(row, i);
+
+        if (!board.hasAnyColConflicts() && !board.hasAnyRowConflicts()) { // if does not have a conflict pass in board to recursive function
+
+          checkBoard(row + 1);
+        }
+        board.togglePiece(row, i);
       }
-      console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-
-      return solution;
-
-    }  
-    for (var i = 0; i < n; i++) {  
-
-      board.togglePiece(row, i);
-
-      if (!board.hasAnyColConflicts() && !board.hasAnyRowConflicts() && solution.length === 0) { // if does not have a conflict pass in board to recursive function
-
-        checkBoard(row + 1);
-      }
-      board.togglePiece(row, i);
     }
      
   };
 
 
-  return checkBoard(0);
-
+  checkBoard(0);
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
